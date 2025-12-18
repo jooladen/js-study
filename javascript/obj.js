f20b15afa258d2348b5e13d9d29e0212f66a5e32
jooladen <68599148+jooladen@users.noreply.github.com>
2025-12-18 03:51:36 +0000

    edit

--- javascript/obj.js
+++ javascript/obj.js
@@ -1,10 +1,22 @@
-const obj = { name: "철수" };
+const obj = { fullname: "철수" };
 
 // 새 키를 만들고 기존 키 삭제
-const newObj = {
-  ...obj,            // 기존 키 복사
-  nickname: obj.name // 새 키에 값 할당
-};
-delete newObj.name;   // 기존 키 삭제
+// const newObj = {
+//   ...obj,            // 기존 키 복사
+//   nickname: obj.fullname // 새 키에 값 할당
+// };
+// delete newObj.fullname;   // 기존 키 삭제
 
-console.log(newObj);  // { nickname: "철수" }
+// console.log(newObj);  // { nickname: "철수" }
+
+//---
+//키를 변수로 만들어서 객체 변경하는 예제
+const keyToChange = "fullname_female";
+const updatedObj = {
+  ...obj,
+  [keyToChange]: "영희" // 변수로 키 지정
+};
+
+
+console.log(updatedObj); // { fullname: "영희" }
+
